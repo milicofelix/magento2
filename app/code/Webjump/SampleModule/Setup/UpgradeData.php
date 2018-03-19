012fc7328b7f1a26e3b413fe18e5aa390593c522
adriano <adriano@webjump.com.br>
2018-03-19 18:29:25 -0300

    Create Schema, create table and populate data and create blocks

---
+++ app/code/Webjump/SampleModule/Setup/UpgradeData.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: adriano
+ * Date: 19/03/18
+ * Time: 10:07
+ */
+namespace Webjump\SampleModule\Setup;
+
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\UpgradeDataInterface;
+
+class UpgradeData implements UpgradeDataInterface
+{
+    public function upgrade(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+       $setup->startSetup();
+
+        if(version_compare($context->getVersion(), '1.0.1', '<')){
+
+            $setup->getConnection()->update(
+                $setup->getTable('webjump_sample_item'),
+                ['description' => 'Default description'],
+                $setup->getConnection()->quoteInto('id = ?',1)
+            );
+        }
+
+
+        $setup->endSetup();
+    }
+}
